b38c39b60040bfc97e176ccbb966b06a6eb208a8
Solon97 <solonb20@gmail.com>
2026-05-12 18:39:58 -0300

    feat(entities): add BaseEntity and UniqueEntityId classes

---
+++ src/domain/entities/base-entity.ts
@@ -0,0 +1,11 @@
+import { UniqueEntityId } from "./value-objects/unique-entity-id";
+
+export class BaseEntity<T> {
+  readonly id: UniqueEntityId;
+  readonly props: T;
+
+  constructor(props: T, id?: string) {
+    this.id = new UniqueEntityId(id);
+    this.props = props;
+  }
+}
